5e20349b584ebf2d7a76dcd419103d9cf0ac0ab8
christinaRas <rasu.christina@gmail.com>
2026-01-20 23:26:13 +0400

    create src

---
+++ auth/src/middleware/auth-middleware.js
@@ -0,0 +1,10 @@
+const authService = require("../service/auth-service");
+
+function requireAuth(req, res, next, isAuthFn = authService.isAuthenticated) {
+  if (!isAuthFn(req)) {
+    return res.status(401).json({ error: "Non authentifié" });
+  }
+  next();
+}
+
+module.exports = { requireAuth };
