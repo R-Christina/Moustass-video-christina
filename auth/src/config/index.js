83a862c3ae9cba31db3283c62fa46a9c347d62b0
christinaRas <rasu.christina@gmail.com>
2026-01-22 20:16:42 +0400

    modify index

--- auth/src/config/index.js
+++ auth/src/config/index.js
@@ -1,20 +1,20 @@
 module.exports = {
-  CLIENT_ID: process.env.CLIENT_ID || 'moustass-client',
-  CLIENT_SECRET: process.env.CLIENT_SECRET || 'your-client-secret',
-  REDIRECT_URI: process.env.REDIRECT_URI || 'http://localhost:3000/callback',
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REDIRECT_URI: process.env.REDIRECT_URI,
   
   // Keycloak URLs
-  KEYCLOAK_ISSUER: process.env.KEYCLOAK_ISSUER || 'http://localhost:8080/realms/moustass',
-  KEYCLOAK_AUTH_URL: process.env.KEYCLOAK_AUTH_URL || 'http://localhost:8080/realms/moustass/protocol/openid-connect/auth',
-  KEYCLOAK_TOKEN_URL: process.env.KEYCLOAK_TOKEN_URL || 'http://keycloak:8080/realms/moustass/protocol/openid-connect/token',
-  KEYCLOAK_USERINFO_URL: process.env.KEYCLOAK_USERINFO_URL || 'http://keycloak:8080/realms/moustass/protocol/openid-connect/userinfo',
+  KEYCLOAK_ISSUER: process.env.KEYCLOAK_ISSUER,
+  KEYCLOAK_AUTH_URL: process.env.KEYCLOAK_AUTH_URL,
+  KEYCLOAK_TOKEN_URL: process.env.KEYCLOAK_TOKEN_URL,
+  KEYCLOAK_USERINFO_URL: process.env.KEYCLOAK_USERINFO_URL,
   
   // Session
-  SESSION_SECRET: process.env.SESSION_SECRET || 'super-secret-change-in-prod',
+  SESSION_SECRET: process.env.SESSION_SECRET,
   
   // Database
-  DB_HOST: process.env.DB_HOST || 'mysql',
-  DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'root',
-  DB_NAME: process.env.DB_NAME || 'moustass_db'
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER ,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME
 };
